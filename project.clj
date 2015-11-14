778081ab6304102d0af2d47802167aa3d3b84130
David RC Dayton <david.rc.dayton@gmail.com>
2015-11-13 17:38:27 -0700

    increment patch number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject enslin/lein-template "0.3.0"
+(defproject enslin/lein-template "0.3.1"
   :description "Desktop app template, for Leiningen, using web technology."
   :url "https://github.com/david-rc-dayton/enslin-template"
   :license {:name "The MIT License (MIT)"
